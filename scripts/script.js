838bb4b438911045ba0712ccb919bd6b80a190a2
Jenzyy <jenzyydev@gmail.com>
2023-06-17 18:32:10 -0500

    add:(view mobile) side navigation menu

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+const btnClose = document.querySelector('.nav__close');
+const btnMenu = document.querySelector('.nav__menu-icon');
+btnMenu.addEventListener('click', toggleMenu);
+btnClose.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  const navMenu = document.querySelector('.nav__menu');
+  if (navMenu.classList.contains('open--menu')) {
+    navMenu.classList.remove('open--menu');
+    return
+  }
+  navMenu.classList.add('open--menu');
+}
